25a2aec8deeb1ff782c0091569a80db6a4fa614f
Vinícius Floriano <viniciushpfloriano@outlook.com>
2023-10-04 07:19:37 -0300

    Initial Commit

---
+++ delivery Chefs/delivery_ChefsApp.swift
@@ -0,0 +1,17 @@
+//
+//  delivery_ChefsApp.swift
+//  delivery Chefs
+//
+//  Created by Vinicius Floriano on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct delivery_ChefsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
